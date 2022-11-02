ee3a51355c01bc828fddffd1c6e976a4621f1dbe
jhpacheco <jhpachecor@gmial.com>
2022-11-02 10:53:54 -0500

    primeros printf

--- Ejemplo01/src/ejemplo01/Ejemplo01.java
+++ Ejemplo01/src/ejemplo01/Ejemplo01.java
@@ -17,7 +17,10 @@ public class Ejemplo01 {
     public static void main(String[] args) {
         // Ejemplo 
         String nombreEstudiante;
-        nombreEstudiante = "René Elizalde";
+        nombreEstudiante = "Jorge Pacheco";
+        System.out.println(nombreEstudiante);
+        System.out.println("Jorge Pacheco");
+        nombreEstudiante = "Jhoel Pacheco";
         System.out.println(nombreEstudiante);
     }
     
--- Ejemplo01/src/ejemplo01/Ejemplo06.java
+++ Ejemplo01/src/ejemplo01/Ejemplo06.java
@@ -14,16 +14,18 @@ public class Ejemplo06 {
         // Uso de printf
         // f: formato
         
-        String nombreEstudiante = "René Rolando";
-        String apellidoEstudiante = "Elizalde Solano";
-        int nacimiento = 1983;
+        String nombreEstudiante = "Jorge Jhoel";
+        String apellidoEstudiante = "Pacheco Requelme";
         
-        // System.out.println(nombreEstudiante+" "+apellidoEstudiante+ " "+ nacimiento);
+        int nacimiento = 2003;
+        
+        // System.out.println(nombreEstudiante+" "+apellidoEstudiante+ " "+ 
+        // nacimiento);
         // En printf; 
         // usamos %s para reemplazar las cadenas, enteros, decimales
         // usamos %d para reemplazar los valores enteros
         // usamos %f para reemplazar los valore decimales
-        System.out.printf("%d\n\n%s\n\n%s\n", nacimiento, apellidoEstudiante, 
-                nacimiento);
+        System.out.printf("Nombre:%s\n\nApellido:%s\n\n%s\n", nombreEstudiante, 
+                apellidoEstudiante, nacimiento);
     }
 }
